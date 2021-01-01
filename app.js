1f2f25a68006cb2a758dc39e3808055142c10102
WilliamTrue <william29854mendoza@gmail.com>
2021-01-01 19:45:27 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+// SELECTORS
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// EVENT LISTENERS
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+
+// FUNCTIONS 
+function addTodo(event) {
+    // Prevent Default
+    event.preventDefault();
+    // Todo DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    // Create LI 
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    // CHECK MARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"> </i>'
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    // CHECK TRASH BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"> </i>'
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+
+    //APPEND TO LIST 
+    todoList.appendChild(todoDiv);
+    //CLEAR todo INPUT VALUE
+    todoInput.value = "";
+    // HASTA AQUI SE PRACTICA LO DEMAS SE PRACTICA LUEGO
+}
+    //DELETE TODO
+function deleteCheck(e) {
+    const item = e.target;
+        if (item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend', function() {
+            todo.remove();
+        });
+    } 
+
+    //CHECK MARK
+
+    if(item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
+
+    
+
